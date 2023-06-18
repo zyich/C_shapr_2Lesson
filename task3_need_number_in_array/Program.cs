98acf49e72138b362609c760ee3d9cc5193b6c33
zyich <zuilinho@yandex.ru>
2023-06-18 16:36:39 +0200

    task3 find need number in array

---
+++ task3_need_number_in_array/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = { 1 , 12, 31, 4, 18, 15, 16, 17, 18 };
+
+int  n = array.Length; // возвращает длину массива (показывает сколько там числе , сслыка на массив)
+int find  =  18 ;
+
+int index = 0 ;
+
+while (index  < n )
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //прервет действие если оно выполнено
+    }
+    index++ ;
+}
